df2b489e8d498e31d922018e4a52a906890ab09e
victorgcampanella <vgcgalvao@outlook.com>
2020-08-25 21:31:55 -0300

    notificando novos agendamentos

--- modulo02/src/app/controllers/AppointmentController.js
+++ modulo02/src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(request, response) {
@@ -81,6 +83,16 @@ class AppointmentController {
       user_id: request.userId,
       provider_id,
       date: hourStart,
+    });
+
+    const user = await User.findByPk(request.userId);
+    const formattedDate = format(hourStart, "dd 'de' MMMM', às' H:mm'h'", {
+      locale: pt,
+    });
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o dia ${formattedDate}`,
+      user: provider_id,
     });
 
     return response.json(appointmennt);
