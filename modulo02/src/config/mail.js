f3e00b63289ba8f42cdd0da8905fe5c2efc05162
victorgcampanella <vgcgalvao@outlook.com>
2020-08-26 21:24:23 -0300

    configurando nodemailer

---
+++ modulo02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '4dc20a5a9936cc',
+    pass: 'b6d07b52056209',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
